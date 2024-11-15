b02cb7685b71d1eec283e8a6c069ac3195a9186c
Gameboi9000 <143445631+Gameboi9000@users.noreply.github.com>
2024-11-15 18:14:05 -0500

    fixed camera
    adjust camera/ added assets/ comm out the castbarscript so no errors occur upon launching unity

--- Assets/Scripts/CastBarTimer.cs
+++ Assets/Scripts/CastBarTimer.cs
@@ -1,41 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class CastBarTimer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        pubic Image castBarFill;
-    public float Keyitem = 1;
-    public float luggitem = 1;
-    public float resitem = 1;
+//public class CastBarTimer : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        pubic Image castBarFill;
+//    public float Keyitem = 1;
+//    public float luggitem = 1;
+//    public float resitem = 1;
 
-    private flat castTime;
-    private bool isCasting = false;
+//    private flat castTime;
+//    private bool isCasting = false;
 
         
 
        
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
 
 
-        if (isCasting)
-        {
+//        if (isCasting)
+//        {
 
-            // cast itemprefab;
-        }
+//            // cast itemprefab;
+//        }
 
-        else
-        {
-            // dont cast anything wait for action 
-        }
-    }
+//        else
+//        {
+//            // dont cast anything wait for action 
+//        }
+//    }
 
-}
-}
+//}
+//}
