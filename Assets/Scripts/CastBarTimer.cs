695bedead009cbe44974d3bb2ce4a64206b8809c
Gameboi9000 <143445631+Gameboi9000@users.noreply.github.com>
2024-11-15 17:37:35 -0500

    Updated assets added

---
+++ Assets/Scripts/CastBarTimer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastBarTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        pubic Image castBarFill;
+    public float Keyitem = 1;
+    public float luggitem = 1;
+    public float resitem = 1;
+
+    private flat castTime;
+    private bool isCasting = false;
+
+        
+
+       
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (isCasting)
+        {
+
+            // cast itemprefab;
+        }
+
+        else
+        {
+            // dont cast anything wait for action 
+        }
+    }
+
+}
+}
